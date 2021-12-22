e0ade48155f44ac7653dc6bc234e2c314f183e3c
Mélusine Rey <zelumine@live.fr>
2021-12-22 18:44:48 +0100

    Trying to find a way to get an element's position

--- src/Components/Tooltip.jsx
+++ src/Components/Tooltip.jsx
@@ -6,49 +6,54 @@ const Tooltip = (props) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
   const [showTooltip, setShowTooltip] = useState(false);
-  let tooltipPosition, arrowPosition;
+  let tooltipPosition, arrowPosition, parentPosition, positionStyle;
 
   const { children, text, position } = props;
 
   useEffect(() => {
-    //console.log("is hovered?", isHovered);
-    //console.log("is hovered?", isHovered);
+    parentPosition = document.getElementById("tooltip").getBoundingClientRect();
+    console.log("parent's position:", parentPosition); 
     if (isHovered || isFocused) setShowTooltip(true);
     else setShowTooltip(false);
   }, [isHovered, isFocused]);
-
-  switch (position) {
-    case 'top': 
-      tooltipPosition = "top";
-      arrowPosition = "bottom";
-      break;
-    case 'right':
-      tooltipPosition = "right";
-      arrowPosition = "left";
-      break;
-    case 'bottom':
-      tooltipPosition = "bottom";
-      arrowPosition = "top";
-      break;
-    case 'left':
-      tooltipPosition = "left";
-      arrowPosition = "right";
-      break;
-    default:
-      tooltipPosition = "top";
-      arrowPosition = "bottom";
-  }
-
+  
+  useEffect(() => {
+    switch (position) {
+      case 'top': 
+        tooltipPosition = "top";
+        arrowPosition = "bottom";
+        break;
+      case 'right':
+        tooltipPosition = "right";
+        arrowPosition = "left";
+        break;
+      case 'bottom':
+        tooltipPosition = "bottom";
+        arrowPosition = "top";
+        positionStyle = `
+          bottom: ${parentPosition.bottom + 10} px
+        `
+        break;
+      case 'left':
+        tooltipPosition = "left";
+        arrowPosition = "right";
+        break;
+      default:
+        tooltipPosition = "top";
+        arrowPosition = "bottom";
+    }
+  }, []);
+  
   return (
-    <div id="tooltip">
+    <div id="tooltip">                              {/* problème à régler : si on utilise plusieurs fois le tooltip on aura plusieurs fois le même ID */}
       <div
         onMouseOver={() => setIsHovered(true)}
         onMouseOut={() => setIsHovered(false)}
         onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}    // remplace le "on focus out"
+        onBlur={() => setIsFocused(false)}          // remplace le "on focus out"
       > 
         {showTooltip &&
-          <div id="tooltip-container" className={`${tooltipPosition} arrow-${arrowPosition}`}>{text}</div>
+          <div id="tooltip-container" style={positionStyle} className={`${tooltipPosition} arrow-${arrowPosition}`}>{text}</div>
         }
         {children}
       </div>
