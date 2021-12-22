d6e52bc5a84a516881308fccc33b112d5419d736
Mélusine Rey <zelumine@live.fr>
2021-12-22 11:04:23 +0100

    Add default arrow to tooltip

--- src/App.js
+++ src/App.js
@@ -6,11 +6,22 @@ import Tooltip from './Components/Tooltip';
 function App() {
   return (
     <div className="App">
-      <Tooltip>
+      <Tooltip 
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ipsum dictum, fermentum nulla vel, posuere turpis. Aenean iaculis ipsum in nisl fringilla faucibus. 
+        Mauris fermentum massa accumsan sapien interdum, id pulvinar odio mollis. Aenean a leo vehicula, scelerisque quam vel, facilisis metus. 
+        Nunc lacinia nisi faucibus mi rhoncus, non interdum dui vehicula. Sed sed maximus massa. Cras tellus eros, tincidunt vel sollicitudin non, vehicula vel est. 
+        Curabitur id sapien laoreet, accumsan lacus vel, varius odio. Donec gravida imperdiet magna, et viverra felis maximus eget."
+      >
         <Button className="red"/>
       </Tooltip>
-      <Tooltip>
+      <br />
+      <Tooltip text="khljkgmljgljkg">
         <Input />
+      </Tooltip>
+      <br />
+      <Tooltip text=" ">
+        <p>Je teste aussi avec du HTML normal</p>
+        <b>Youhou</b>
       </Tooltip>
     </div>
   );
--- src/Components/Tooltip.jsx
+++ src/Components/Tooltip.jsx
@@ -6,15 +6,38 @@ const Tooltip = (props) => {
   const [isHovered, setIsHovered] = useState(false);
   const [isFocused, setIsFocused] = useState(false);
   const [showTooltip, setShowTooltip] = useState(false);
+  let tooltipPosition, arrowPosition;
 
-  const { children } = props;
+  const { children, text, position } = props;
 
   useEffect(() => {
-    console.log("is hovered?", isHovered);
-    console.log("is hovered?", isHovered);
+    //console.log("is hovered?", isHovered);
+    //console.log("is hovered?", isHovered);
     if (isHovered || isFocused) setShowTooltip(true);
     else setShowTooltip(false);
   }, [isHovered, isFocused]);
+
+  switch (position) {
+    case 'top': 
+      tooltipPosition = "top";
+      arrowPosition = "bottom";
+      break;
+    case 'right':
+      tooltipPosition = "right";
+      arrowPosition = "left";
+      break;
+    case 'bottom':
+      tooltipPosition = "bottom";
+      arrowPosition = "top";
+      break;
+    case 'left':
+      tooltipPosition = "left";
+      arrowPosition = "right";
+      break;
+    default:
+      tooltipPosition = "top";
+      arrowPosition = "bottom";
+  }
 
   return (
     <div id="tooltip">
@@ -22,9 +45,10 @@ const Tooltip = (props) => {
         onMouseOver={() => setIsHovered(true)}
         onMouseOut={() => setIsHovered(false)}
         onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}    // remplace le "on focus out"
       > 
         {showTooltip &&
-          <div>Coucou c'est le tooltip</div>
+          <div id="tooltip-container" className={`${tooltipPosition} arrow-${arrowPosition}`}>{text}</div>
         }
         {children}
       </div>
