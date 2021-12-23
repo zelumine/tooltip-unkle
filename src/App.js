3ff20e7b669983f139e085bae9fb954314a2947d
Mélusine Rey <zelumine@live.fr>
2021-12-23 11:46:01 +0100

    Revert "Change the layout a bit and add arrow-bottom class"
    This reverts commit 7733bd1b0081b2db49ec987ab1e15b3d89bf0659.

--- src/App.js
+++ src/App.js
@@ -6,27 +6,22 @@ import Tooltip from './Components/Tooltip';
 function App() {
   return (
     <div className="App">
-      <h1>Test tooltip</h1>
       <Tooltip 
-        id="tooltip-1"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel ipsum dictum, fermentum nulla vel, posuere turpis. Aenean iaculis ipsum in nisl fringilla faucibus. 
         Mauris fermentum massa accumsan sapien interdum, id pulvinar odio mollis. Aenean a leo vehicula, scelerisque quam vel, facilisis metus. 
         Nunc lacinia nisi faucibus mi rhoncus, non interdum dui vehicula. Sed sed maximus massa. Cras tellus eros, tincidunt vel sollicitudin non, vehicula vel est. 
         Curabitur id sapien laoreet, accumsan lacus vel, varius odio. Donec gravida imperdiet magna, et viverra felis maximus eget."
       >
-        <label>Nom</label> <br />
-        <input type="text" />
+        <Button className="red"/>
       </Tooltip>
       <br />
-      {/* Test avec un composant */}
-      <Tooltip id="tooltip-2" text="khljkgmljgljkg">
-        <label>Prénom</label>
+      <Tooltip text="khljkgmljgljkg">
         <Input />
       </Tooltip>
       <br />
-      {/* Test avec un composant et pas de texte de tooltip*/}
-      <Tooltip id="tooltip-3" text=" " position="bottom">
-        <Button />
+      <Tooltip text=" ">
+        <p>Je teste aussi avec du HTML normal</p>
+        <b>Youhou</b>
       </Tooltip>
     </div>
   );
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = () => {
   return (
     <button>
-      Continuer
+      Click me!
     </button>
   )
 }
